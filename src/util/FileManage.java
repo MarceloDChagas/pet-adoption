398853a055f6c2be6205d7f503c9962f6cc76729
MarceloDChagas <mlcr1@discente.ifpe.edu.br>
2025-03-12 08:38:16 -0300

    feat: Passo 1

---
+++ src/util/FileManage.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManage {
+    protected List<String> readFile(String filePath) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    protected void writeFile(String filePath, List<String> lines) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
